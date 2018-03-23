350384a39ea5aaff847daac1971bc64472ade714
sclaudon <stephane.claudon@gmail.com>
2018-03-23 15:14:31 +0100

    snapshot

---
+++ src/components/projects-slider-item.vue
@@ -0,0 +1,307 @@
+<template>
+    <div :id="wrapperId" class="canvas-video-wrapper projects-slider-item">
+        <div class="projects-slider-item__bg grid-x align-middle align-center" :class="{snapshoted: snapshoted}">
+            <!--<img :src="getVideoPoster(project)" :alt="project.title">-->
+            <!--<canvas-video :alive="isCurrentIndex(index)" :name="project.id" :title="project.title"></canvas-video>-->
+            <div>
+                <video :id="'video-' + project.id" playsinline loop muted autoplay>
+                    <source :src="getVideoPath()" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+                <canvas :id="'canvas-' + project.id" width="200" height="200"></canvas>
+            </div>
+        </div>
+        <!--<div class="projects-slider-item__title grid-x align-middle">
+            <div class="cell small-9 small-offset-1">
+                <h1 v-if="alive">
+                    <span class="title--inner" v-for="(titleString, titleIndex) in projectArrayTitle" :key="titleIndex"><span>{{ titleString }}</span></span>
+                </h1>
+
+                <div v-if="!titleComputed" class="title--innerfake"><span :id="project.id" :class="projectIndex">{{ project.title }}</span></div>
+            </div>
+        </div>-->
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+//@ts-ignore
+import * as PIXI from "pixi.js";
+
+@Component
+export default class ProjectsSliderItem extends Vue {
+  projectArrayTitle: Array<string> = [];
+  titleComputed: boolean = false;
+  videoElement: HTMLVideoElement;
+  canvasElement: HTMLCanvasElement;
+  canvasContext: CanvasRenderingContext2D;
+  snapshot: string = "";
+  snapshoted: boolean = false;
+  inited: boolean = false;
+
+  loopsPath: string = "dist/assets/loops/";
+  wrapperId: string = "canvas-video-wrapper_";
+  pixiTicker: any = PIXI.ticker.shared;
+  videoTexture: PIXI.Texture;
+
+  @Prop() project: any;
+  @Prop() projectIndex: number;
+  @Prop({ default: false })
+  alive: boolean;
+  @Prop({ default: false })
+  moving: boolean;
+
+  created() {
+    this.wrapperId = this.wrapperId + this.project.name;
+  }
+
+  getVideoPath(): String {
+    return "/dist/assets/loops/" + this.project.id + ".mp4";
+  }
+
+  checkTitlesLineBreak() {
+    let currentPorject: any = this.project;
+    let currentHtmlTitle: Element = document.getElementById(
+      currentPorject.id
+    ) as Element;
+
+    if (currentHtmlTitle !== null) {
+      let text: string = currentHtmlTitle.textContent as string;
+      let words = text.split(" ");
+      currentHtmlTitle.textContent = words[0];
+      let height = currentHtmlTitle.getBoundingClientRect().height;
+      let currentTitleArray: Array<string> = [];
+      let lastFoundWordIndex: number = 0;
+
+      for (let i = 1; i < words.length; i++) {
+        currentHtmlTitle.textContent =
+          currentHtmlTitle.textContent + " " + words[i];
+
+        if (currentHtmlTitle.getBoundingClientRect().height > height) {
+          height = currentHtmlTitle.getBoundingClientRect().height;
+
+          currentTitleArray.push(words.slice(lastFoundWordIndex, i).join(" "));
+          lastFoundWordIndex = i;
+        }
+      }
+      currentTitleArray.push(
+        words.slice(lastFoundWordIndex, words.length).join(" ")
+      );
+      this.projectArrayTitle = currentTitleArray;
+    }
+    this.titleComputed = true;
+  }
+
+  toggleVideo() {
+    if (this.alive) {
+      this.videoTexture.baseTexture.source.play();
+      this.pixiTicker.start();
+    } else {
+      this.videoTexture.baseTexture.source.pause();
+      this.pixiTicker.stop();
+    }
+  }
+
+  mounted() {
+    this.checkTitlesLineBreak();
+    this.videoElement = document.getElementById(
+      "video-" + this.project.id
+    ) as HTMLVideoElement;
+
+    this.canvasElement = document.getElementById(
+      "canvas-" + this.project.id
+    ) as HTMLCanvasElement;
+    this.videoElement.addEventListener(
+      "playing",
+      this.onVideoStartPlaying,
+      false
+    );
+    this.canvasContext = this.canvasElement.getContext("2d") as CanvasRenderingContext2D;
+  }
+
+  onVideoStartPlaying() {
+    console.log('video playing -- ', this.project.title);
+      
+    if(!this.inited) {
+        if (!this.alive) {
+            this.videoElement.pause();
+            this.freezeVideo();
+        } 
+        this.inited = true;
+    }
+  }
+
+  freezeVideo() {
+      this.canvasElement.width = this.videoElement.videoWidth;
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasContext!.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      this.videoElement.pause();
+      this.snapshoted = true;
+
+  }
+
+  unfreezeVideo() {
+      this.videoElement.play();
+      this.snapshoted = false;
+      this.canvasContext.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+  }
+
+  @Watch("alive", { immediate: false, deep: false })
+  onAliveChanged(val: boolean, oldVal: boolean) {
+    if(val)
+        this.unfreezeVideo();
+  }
+
+  @Watch("moving", { immediate: false, deep: false })
+  onMovingChanged(val: boolean, oldVal: boolean) {
+    if(val !== oldVal && val)
+        this.freezeVideo();
+    else if(this.alive)
+        this.unfreezeVideo();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/main.scss";
+$titleAnimationDuration: 0.75s;
+$titleAnimationMultilineDelay: 0.15s;
+
+.canvas-video-wrapper {
+  height: 100%;
+  width: 100%;
+}
+
+.projects-slider-item {
+  height: 100%;
+  &__bg {
+    height: 100%;
+    overflow: hidden;
+    img {
+      height: 100%;
+    }
+    video {
+      visibility: block;
+    }
+    canvas {
+      visibility: none;
+    }
+    &.snapshoted {
+      video {
+        display: none;
+      }
+      canvas {
+        display: block;
+      }
+    }
+  }
+  &__title {
+    position: absolute;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    text-align: left;
+    .cell {
+      margin-top: 35%;
+    }
+    .title--inner,
+    .title--innerfake {
+      position: relative;
+      display: inline-block;
+      z-index: 200;
+      padding: 0.5em;
+      vertical-align: middle;
+      text-transform: uppercase;
+      font-size: 2em;
+      span {
+        @include roboto-black;
+        letter-spacing: 0.05em;
+      }
+    }
+    .title--innerfake {
+      visibility: hidden;
+    }
+    .title--inner {
+      overflow: hidden;
+      span {
+        visibility: hidden;
+        @keyframes textAnim {
+          from {
+            visibility: hidden;
+          }
+          to {
+            visibility: visible;
+          }
+        }
+      }
+      &::after {
+        position: absolute;
+        display: block;
+        top: -1px;
+        right: 100%;
+        bottom: -1px;
+        left: -1%;
+        content: "";
+        background: $black;
+
+        @keyframes blackBG {
+          0% {
+            right: 100%;
+          }
+          50% {
+            right: 0%;
+            left: -1%;
+          }
+          100% {
+            right: 0%;
+            left: 100%;
+          }
+        }
+        @include animation-timing-function(cubic-bezier(0.55, 0, 0.28, 1));
+      }
+      &::before {
+        position: absolute;
+        visibility: hidden;
+        z-index: -1;
+        top: -1px;
+        right: 0%;
+        bottom: 0px;
+        left: -1%;
+        content: "";
+        @include horizontal-gradient($blue, $purple);
+
+        @keyframes gradientBG {
+          from {
+            visibility: hidden;
+          }
+          to {
+            visibility: visible;
+          }
+        }
+      }
+    }
+    .title--inner:not(:last-child)::before {
+      border-bottom: solid 1px $black;
+    }
+    @for $i from 0 through 4 {
+      .title--inner:nth-child(#{$i}) {
+        span {
+          @include animation(#{$i * $titleAnimationMultilineDelay + $titleAnimationDuration * 0.5}, 0.01s, textAnim);
+        }
+        &::before {
+          @include animation(#{$i * $titleAnimationMultilineDelay+$titleAnimationDuration * 0.5}, 0.01s, gradientBG);
+        }
+        &::after {
+          @include animation(#{$i * $titleAnimationMultilineDelay}, $titleAnimationDuration, blackBG);
+        }
+      }
+    }
+  }
+}
+</style>
--- src/components/projects-slider.vue
+++ src/components/projects-slider.vue
@@ -3,25 +3,7 @@
         <div class="projects swipe-wrap" >
             <div v-for="(project, index) in projects" :key="project.id" class="projects--item">
                 <a @click="viewProject(project)" href="#">
-                    <div class="projects--item__bg grid-x align-middle align-center">
-                        <!--<img :src="getVideoPoster(project)" :alt="project.title">-->
-                        <!--<canvas-video :alive="isCurrentIndex(index)" :name="project.id" :title="project.title"></canvas-video>-->
-                        <div>
-                            <video playsinline loop muted autoplay>
-                                <source :src="getVideoPath(project)" type="video/mp4">
-                                Your browser does not support the video tag.
-                            </video>
-                        </div>
-                    </div>
-                    <div class="projects--item__title grid-x align-middle">
-                        <div class="cell small-9 small-offset-1">
-                            <h1 v-if="isCurrentIndex(index)">
-                                <span class="title--inner" v-for="(titleString, titleIndex) in projectsArrayTitle[index]" :key="titleIndex"><span>{{ titleString }}</span></span>
-                            </h1>
-
-                            <div v-if="!titlesComputed" class="title--innerfake"><span :id="project.id" :class="index">{{ project.title }}</span></div>
-                        </div>
-                    </div>
+                    <projects-slider-item :project="project" :project-index="index" :alive="isCurrentIndex(index)" :moving="sliderIsMoving"></projects-slider-item>
                 </a>
             </div>
         </div>
@@ -33,13 +15,13 @@
 import { State, Mutation } from "vuex-class";
 import { Vue, Component, Prop, Emit } from "vue-property-decorator";
 import * as MutationTypes from "../store/mutation-types";
+import ProjectsSliderItem from './projects-slider-item.vue';
 //@ts-ignore
-import * as SwipeJS from "swipejs";
-import CanvasVideo from './canvas-video.vue'
+import * as SwipeJS from "../lib/swipeJs/swipe.js";
 
 @Component({
     components: {
-        CanvasVideo
+        ProjectsSliderItem
     }
 })
 export default class ProjectsSlider extends Vue {
@@ -52,16 +34,11 @@ export default class ProjectsSlider extends Vue {
   projectsSwipe: Object = {};
   currentIndex: number = 0;
   titlesComputed: boolean = false;
+  sliderIsMoving: boolean = false;
 
   viewProject(project: any): void {
     this.setCurrentProject(project);
     console.log("View project " + project.title);
-  }
-
-
-
-  getVideoPath(project: any): String {
-    return "/dist/assets/loops/" + project.id + ".mp4";
   }
 
   checkTitlesLineBreak() {
@@ -102,7 +79,7 @@ export default class ProjectsSlider extends Vue {
   }
 
   mounted() {
-    this.checkTitlesLineBreak();
+    //this.checkTitlesLineBreak();
 
     this.projectsSwipe = SwipeJS(
       document.getElementById("slider") as HTMLElement,
@@ -115,13 +92,18 @@ export default class ProjectsSlider extends Vue {
         disableScroll: false,
         stopPropagation: true,
         callback: this.onSliderInited,
-        transitionEnd: this.onSliderTransitionEnd
+        transitionEnd: this.onSliderTransitionEnd,
+        transitionStart: this.onSliderTransitionStart
       }
     ) as Object;
   }
 
   onSliderInited(index: number, elem: HTMLElement, dir: number): void {}
+  onSliderTransitionStart(): void {
+    this.sliderIsMoving = true;
+  }
   onSliderTransitionEnd(index: number, elem: HTMLElement): void {
+    this.sliderIsMoving = false;
     this.currentIndex = index;
   }
 
@@ -132,126 +114,11 @@ export default class ProjectsSlider extends Vue {
 </script>
 
 <style lang="scss">
-@import "../style/main.scss";
-$titleAnimationDuration: 0.75s;
-$titleAnimationMultilineDelay: 0.15s;
 
 #slider {
   height: 100%;
 }
 .projects {
   height: 100%;
-  &--item {
-    height: 100%;
-    &__bg {
-      height: 100%;
-      overflow: hidden;
-      img {
-        height: 100%;
-      }
-    }
-    &__title {
-      position: absolute;
-      top: 0;
-      height: 100%;
-      width: 100%;
-      text-align: left;
-      .cell {
-        margin-top: 35%;
-      }
-      .title--inner,
-      .title--innerfake {
-        position: relative;
-        display: inline-block;
-        z-index: 200;
-        padding: 0.5em;
-        vertical-align: middle;
-        text-transform: uppercase;
-        font-size: 2em;
-        span {
-          @include roboto-black;
-          letter-spacing: 0.05em;
-        }
-      }
-      .title--innerfake {
-        visibility: hidden;
-      }
-      .title--inner {
-        overflow: hidden;
-        span {
-          visibility: hidden;
-          @keyframes textAnim {
-            from {
-              visibility: hidden;
-            }
-            to {
-              visibility: visible;
-            }
-          }
-        }
-        &::after {
-          position: absolute;
-          display: block;
-          top: -1px;
-          right: 100%;
-          bottom: -1px;
-          left: -1%;
-          content: "";
-          background: $black;
-
-          @keyframes blackBG {
-            0% {
-              right: 100%;
-            }
-            50% {
-              right: 0%;
-              left: -1%;
-            }
-            100% {
-              right: 0%;
-              left: 100%;
-            }
-          }
-          @include animation-timing-function(cubic-bezier(0.55, 0, 0.28, 1));
-        }
-        &::before {
-          position: absolute;
-          visibility: hidden;
-          z-index: -1;
-          top: -1px;
-          right: 0%;
-          bottom: 0px;
-          left: -1%;
-          content: "";
-          @include horizontal-gradient($blue, $purple);
-
-          @keyframes gradientBG {
-            from {
-              visibility: hidden;
-            }
-            to {
-              visibility: visible;
-            }
-          }
-        }
-      }
-      .title--inner:not(:last-child)::before {
-        border-bottom: solid 1px $black;
-      }
-      @for $i from 0 through 4 {
-        .title--inner:nth-child(#{$i}) {
-          span {
-            @include animation(#{$i * $titleAnimationMultilineDelay + $titleAnimationDuration * 0.5}, 0.01s, textAnim);
-          }
-          &::before {
-            @include animation(#{$i * $titleAnimationMultilineDelay+$titleAnimationDuration * 0.5}, 0.01s, gradientBG);
-          }
-          &::after {
-            @include animation(#{$i * $titleAnimationMultilineDelay}, $titleAnimationDuration, blackBG);
-          }
-        }
-      }
-    }
-  }
 }
 </style>
