e608bd47d76f719724a52ec35cf5d2c057578ddb
sclaudon <stephane.claudon@gmail.com>
2018-05-02 09:39:24 +0200

    added vignette

--- src/components/projects-slider-canvas.vue
+++ src/components/projects-slider-canvas.vue
@@ -176,7 +176,7 @@ export default class ProjectsSliderCanvas extends Vue {
       linear-gradient(90deg, rgba(0,0,0,.5) 1px, transparent 1px),
       radial-gradient(transparent, rgba(0,0,0,0.8));
       background-size: 2px 2px, 2px 2px, 2px 2px, 2px 2px;*/
-      background-image: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
+      background-image: radial-gradient(transparent, rgba(0,0,0,0.5)), url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
     }
 
     &-titles {
