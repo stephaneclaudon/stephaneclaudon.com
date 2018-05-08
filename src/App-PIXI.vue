2c0a00d168438e16a4d3ccddf31bdf80520e3768
sclaudon <stephane.claudon@gmail.com>
2018-05-08 14:38:58 +0200

    working vertical pixel sorting

--- src/App-PIXI.vue
+++ src/App-PIXI.vue
@@ -1,47 +1,6 @@
 <template>
   <div id="app" class="app">
-    <script id="fragShader" type="shader-code">
-      precision mediump float;
-
-varying vec2 vTextureCoord;
-
-uniform vec2 size;
-uniform sampler2D uSampler;
-
-uniform vec4 filterArea;
-
-vec2 mapCoord( vec2 coord )
-{
-    coord *= filterArea.xy;
-    coord += filterArea.zw;
-
-    return coord;
-}
-
-vec2 unmapCoord( vec2 coord )
-{
-    coord -= filterArea.zw;
-    coord /= filterArea.xy;
-
-    return coord;
-}
-
-vec2 pixelate(vec2 coord, vec2 size)
-{
-	return floor( coord / size ) * size;
-}
-
-void main(void)
-{
-    vec2 coord = mapCoord(vTextureCoord);
-
-    coord = pixelate(coord, size);
-
-    coord = unmapCoord(coord);
-
-    gl_FragColor = texture2D(uSampler, coord);
-}
-</script>
+    
   </div>
 </template>
 
@@ -64,22 +23,27 @@ export default class AppPIXI extends Vue {
 
   initPIXI() {
 
-    this.pixiApp = new PIXI.Application(window.innerWidth, window.innerHeight);
+    this.pixiApp = new PIXI.Application();
     document.getElementById("app")!.appendChild(this.pixiApp.view);
 
     this.sprite = PIXI.Sprite.fromImage(
-      "https://raw.githubusercontent.com/stephaneclaudon/stephaneclaudon.com/master/src/assets/img/nautilus%403x.jpg"
+      "https://raw.githubusercontent.com/stephaneclaudon/stephaneclaudon.com/master/src/assets/img/awpc%403x.jpg"
+      //"https://raw.githubusercontent.com/stephaneclaudon/stephaneclaudon.com/master/src/assets/img/nautilus%403x.jpg"
     );
     this.sprite.anchor.set(0.5);
     this.sprite.x = this.pixiApp.screen.width / 2;
     this.sprite.y = this.pixiApp.screen.height / 2;
+    this.sprite.width = this.pixiApp.screen.width;
+    this.sprite.height = this.pixiApp.screen.height;
 
     this.pixiApp.stage.addChild(this.sprite);
     this.initFilter();
 
     this.pixiApp.ticker.add(delta => {
-      //this.smokeShader.size += 0.5;
-      this.smokeShader.size = this.pixiApp.renderer.plugins.interaction.mouse.global.x - (window.innerWidth * 0.5);
+      //this.smokeShader.size += 0.1;
+      this.smokeShader.size = (this.pixiApp.renderer.plugins.interaction.mouse.global.x - (this.pixiApp.screen.width * 0.5));
+      console.log(this.smokeShader.size);
+      
       
     });
   }
@@ -90,8 +54,8 @@ export default class AppPIXI extends Vue {
     var uniforms: any = {};
     uniforms.size = { type: "v2", value: { x: 1, y: 1 } };
 
-    var shaderCode = document.getElementById("fragShader")!.innerHTML;
     this.smokeShader = new PixelSortingFilter(0);
+    this.smokeShader.uniforms.iResolution = [this.pixiApp.screen.width, this.pixiApp.screen.height];
     this.pixiApp.stage.filters = [this.smokeShader];
     console.log(this.smokeShader.uniforms);
 
